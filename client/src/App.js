d3a21ff4547a3c8a6f812b140d4b239c8cad7915
Rene Cruz <renecruzatx@outlook.com>
2019-01-29 20:31:05 -0600

    Minor fixes and lazy loading certain components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import {Route, Switch, withRouter, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index'; 
+
+const asyncCheckout = asyncComponent(()=>{
+  return import('./containers/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(()=>{
+  return import('./containers/Orders/Orders');
+});
+
+const asyncAuth = asyncComponent(()=>{
+  return import('./containers/Auth/Auth')
+});
 
 class App extends Component {
   //checks to see if user is already logged in
@@ -20,7 +30,7 @@ class App extends Component {
     //renders routes that don't need authentication
     let routes = (
       <Switch>
-        <Route path="/login" component={Auth}/>
+        <Route path="/login" component={asyncAuth}/>
         <Route path="/" exact component={BurgerBuilder}/>
         <Redirect to="/"/>
       </Switch>
@@ -29,9 +39,10 @@ class App extends Component {
     if (this.props.isAuthenticated) {
       routes = (
         <Switch>
-          <Route path="/checkout" component={Checkout}/>
-          <Route path="/orders" component={Orders}/>
+          <Route path="/checkout" component={asyncCheckout}/>
+          <Route path="/orders" component={asyncOrders}/>
           <Route path="/logout" component={Logout}/>
+          <Route path="/login" component={asyncAuth}/>
           <Route path="/" exact component={BurgerBuilder}/>
           <Redirect to="/"/>
         </Switch>
