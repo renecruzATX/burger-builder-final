ddd86e9cd079675f95b0c5d4f28105993c118129
Rene Cruz <renecruzATX@outlook.com>
2019-05-08 21:50:14 -0500

    heroku changes

---
+++ server/public/precache-manifest.f5634d3210a24c1ac4846c498e0a593f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1d15fdfe854318c72f41",
+    "url": "/static/js/main.1d15fdfe.chunk.js"
+  },
+  {
+    "revision": "a7cb7655c3e4d89b022d",
+    "url": "/static/js/1.a7cb7655.chunk.js"
+  },
+  {
+    "revision": "1d15fdfe854318c72f41",
+    "url": "/static/css/main.f1d409ed.chunk.css"
+  },
+  {
+    "revision": "a0edccb6cb98a7ee2e02d26668e75deb",
+    "url": "/index.html"
+  }
+];
